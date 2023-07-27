1b1b72e10e07c2c6f311f252ef8953fd736a7a8e
denniszielke <11569044+denniszielke@users.noreply.github.com>
2023-07-27 20:54:48 +0200

    added ui

---
+++ labs/04-deploy-ai/02-aca/app.py
@@ -0,0 +1,60 @@
+import chainlit as cl
+from langchain import PromptTemplate, OpenAI, LLMChain
+from langchain.llms import AzureOpenAI
+import os
+
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+openai_api_type = os.getenv("OPENAI_API_TYPE")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+openai_api_base = os.getenv("OPENAI_API_BASE")
+openai_api_version = os.getenv("OPENAI_API_VERSION")
+deployment_name = os.getenv("AZURE_OPENAI_COMPLETION_DEPLOYMENT_NAME")
+embedding_name = os.getenv("AZURE_OPENAI_EMBEDDING_DEPLOYMENT_NAME")
+
+settings = {
+    "temperature": 0,
+    "max_tokens": 500,
+    "top_p": 1,
+    "frequency_penalty": 0,
+    "presence_penalty": 0,
+    "stop": ["```"]
+}
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+@cl.on_chat_start
+def main():
+    # Instantiate the chain for that user session
+    llmAzureOpenAI = AzureOpenAI(
+        openai_api_type = openai_api_type,
+        openai_api_version = openai_api_version,
+        openai_api_base = openai_api_base,
+        openai_api_key = openai_api_key,
+        deployment_name = deployment_name,
+        temperature = 0
+    )
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llmAzureOpenAI, verbose=True)
+
+    # Store the chain in the user session
+    cl.user_session.set("llm_chain", llm_chain)
+
+
+@cl.on_message
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+
+    # Call the chain asynchronously
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    # Do any post processing here
+
+    # Send the response
+    await cl.Message(content=res["text"]).send()
---
+++ labs/04-deploy-ai/02-aca/chainlit.md
@@ -0,0 +1,11 @@
+# Welcome to Chainlit! 🚀🤖
+
+Hi there, Developer! 👋 We're excited to have you on board. Chainlit is a powerful tool designed to help you prototype, debug and share applications built on top of LLMs.
+
+## Useful Links 🔗
+
+- **Documentation:** Get started with our comprehensive [Chainlit Documentation](https://docs.chainlit.io) 📚
+- **Discord Community:** Join our friendly [Chainlit Discord](https://discord.gg/ZThrUxbAYw) to ask questions, share your projects, and connect with other developers! 💬
+
+We can't wait to see what you create with Chainlit! Happy coding! 💻😊
+
